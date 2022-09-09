6302c7c00701be5c3b023d8b11dd008de7621663
root <root@localhost.localdomain>
2022-09-09 13:24:51 +0800

    ...

--- utils/Utils.py
+++ utils/Utils.py
@@ -1,11 +1,21 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple, Union
+from CustomException import InvalidPosition
 
+
+pos_dict = {
+            "header": [0],
+            "footer": [-1],
+        }
 
 class Utils:
-
-    def __init__(self, data_linked_obj, pos):
-        self._data_linked_list = data_linked_obj
-        self.pos = pos
+    
+    def __init__(self, xml_data_obj, pos: Union[str, List[int], Tuple[int]] = "footer"):
+        try:
+            self.pos = pos_dict[pos] if type(pos) is str else pos
+        except KeyError:
+            raise InvalidPosition(f"\'{pos}\' is not a valid position;valid pos: {','.join(i for i in pos_dict.keys())}")
+        
+        self.__xml_data_obj = xml_data_obj
 
     def add_dialogue(self, character_name: str, dialogue: str) -> bool:
         pass
@@ -23,7 +33,7 @@ class Utils:
                             font_style_path: str, name_tag: str = "default") -> bool:
         pass
 
-    def add_transform(self, type_list: List(str), **args) -> bool:
+    def add_transform(self, type_list: List[str], **args) -> bool:
         pass
 
     def get_data_linked_list(self):
